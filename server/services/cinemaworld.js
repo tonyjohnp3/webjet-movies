f5bdf90415a5189c8ad6f10546a6282e2033851a
Tony John <tony.john@iselect.com.au>
2020-01-07 03:47:03 +1100

    Update list method and add getdetails method

--- server/services/cinemaworld.js
+++ server/services/cinemaworld.js
@@ -1,25 +1,65 @@
 import axios from 'axios';
+import statusCodes from '../constants/statusCodes';
+import ApiError from '../errors/apiError';
 
 class Cinemaworld {
-    constructor() {
-        this.instance = axios.create({
-            baseURL: 'http://webjetapitest.azurewebsites.net/api/cinemaworld',
-            timeout: 4000,
-            headers: {
-                'x-access-token': process.env.MOVIE_ACCESS_TOKEN
-            }
-        });
-    }
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://webjetapitest.azurewebsites.net/api/cinemaworld',
+      timeout: 4000,
+      headers: {
+        'x-access-token': process.env.MOVIE_ACCESS_TOKEN,
+      },
+    });
+  }
 
-    list() {
-        const url = '/movies'; 
-        return this.instance.get(url);
-    }
+  async list() {
+    try {
+      const url = '/movies';
+      const result = await this.instance.get(url);
 
-    getDetails(id) {
-        const url = `/movie/${id}`;
-        return this.instance.get(url);
+      return {
+        status: result.status,
+        data: result.data,
+      };
+    } catch (err) {
+      if (
+        err.hasOwnProperty('response') &&
+        typeof err.response === 'object' &&
+        err.response.hasOwnProperty('status')
+      ) {
+        console.log('cinemaworld 400 or 500', err);
+        throw new ApiError(err.message, err.response.status); // internal server error or invalid request
+      } else {
+        console.log('cinemaworld timeout', err);
+        throw new ApiError(err.message, statusCodes.INTERNAL_SERVER_ERROR); // timedout, try again
+      }
     }
+  }
+
+  async getDetails(id) {
+    try {
+      const url = `/movie/${id}`;
+      const result = await this.instance.get(url);
+
+      return {
+        status: result.status,
+        data: result.data,
+      };
+    } catch (err) {
+      if (
+        err.hasOwnProperty('response') &&
+        typeof err.response === 'object' &&
+        err.response.hasOwnProperty('status')
+      ) {
+        console.log('cinemaworld get details 400 or 500', err);
+        throw new ApiError(err.message, err.response.status); // internal server error or invalid request
+      } else {
+        console.log('cinemaworld get details timeout', err);
+        throw new ApiError(err.message, statusCodes.INTERNAL_SERVER_ERROR); // timedout, try again
+      }
+    }
+  }
 }
 
 export default Cinemaworld;
--- server/services/filmworld.js
+++ server/services/filmworld.js
@@ -1,25 +1,65 @@
 import axios from 'axios';
+import statusCodes from '../constants/statusCodes';
+import ApiError from '../errors/apiError';
 
 class Filmworld {
-    constructor() {
-        this.instance = axios.create({
-            baseURL: 'http://webjetapitest.azurewebsites.net/api/filmworld',
-            timeout: 4000,
-            headers: {
-                'x-access-token': process.env.MOVIE_ACCESS_TOKEN
-            }
-        });
-    }
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'http://webjetapitest.azurewebsites.net/api/filmworld',
+      timeout: 4000,
+      headers: {
+        'x-access-token': process.env.MOVIE_ACCESS_TOKEN,
+      },
+    });
+  }
 
-    list() {
-        const url = '/movies'; 
-        return this.instance.get(url);
-    }
+  async list() {
+    try {
+      const url = '/movies';
+      const result = await this.instance.get(url);
 
-    getDetails(id) {
-        const url = `/movie/${id}`;
-        return this.instance.get(url);
+      return {
+        status: result.status,
+        data: result.data,
+      };
+    } catch (err) {
+      if (
+        err.hasOwnProperty('response') &&
+        typeof err.response === 'object' &&
+        err.response.hasOwnProperty('status')
+      ) {
+        console.log('filmworld 400 or 500', err);
+        throw new ApiError(err.message, err.response.status); // internal server error or invalid request
+      } else {
+        console.log('filmworld timeout', err);
+        throw new ApiError(err.message, statusCodes.INTERNAL_SERVER_ERROR); // timedout, try again
+      }
     }
+  }
+
+  async getDetails(id) {
+    try {
+      const url = `/movie/${id}`;
+      const result = await this.instance.get(url);
+
+      return {
+        status: result.status,
+        data: result.data,
+      };
+    } catch (err) {
+      if (
+        err.hasOwnProperty('response') &&
+        typeof err.response === 'object' &&
+        err.response.hasOwnProperty('status')
+      ) {
+        console.log('filmworld get details 400 or 500', err);
+        throw new ApiError(err.message, err.response.status); // internal server error or invalid request
+      } else {
+        console.log('filmworld get details timeout', err);
+        throw new ApiError(err.message, statusCodes.INTERNAL_SERVER_ERROR); // timedout, try again
+      }
+    }
+  }
 }
 
 export default Filmworld;
