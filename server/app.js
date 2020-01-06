9ed1c749cf2845b966f8214c182f34e00ea2b8d0
Tony John <tony.john@iselect.com.au>
2020-01-07 03:48:30 +1100

    Add endpoint for getdetials

--- server/app.js
+++ server/app.js
@@ -14,13 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.use('/movies', Movies.list);
+app.use('/movie/fwid/:fwid/cwid/:cwid', Movies.getDetails);
 
 export default app;
