bdb5ea651827999134d528a98405907824709f44
Tony John <tony.john@iselect.com.au>
2020-01-07 03:48:03 +1100

    Update list endpoint and add getdetails endpoint

--- server/api/movies.js
+++ server/api/movies.js
@@ -1,7 +1,29 @@
+import Cinemaworld from '../services/cinemaworld';
+import Filmworld from '../services/filmworld';
+import Movie from '../models/movie';
+import statusCodes from '../constants/statusCodes';
+
 const Movies = {
-    list(req, res) {
-        res.send(`Recieved : ${req.query.test}`);
-    },
+  async list(req, res) {
+    try {
+      const movie = new Movie([new Filmworld(), new Cinemaworld()]);
+      const result = await movie.fetchAll();
+      res.send(result);
+    } catch (err) {
+      res.status(statusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+    }
+  },
+
+  async getDetails(req, res) {
+    try {
+      const ids = [req.params.fwid, req.params.cwid];
+      const movie = new Movie([new Filmworld(), new Cinemaworld()]);
+      const result = await movie.getDetails(ids);
+      res.send(result);
+    } catch (err) {
+      res.status(statusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+    }
+  },
 };
 
 export default Movies;
